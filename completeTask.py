d128aea844f22cdfc7ccb054cbf47ab07172d28a
Radhika7-ai <70372793+Radhika7-ai@users.noreply.github.com>
2022-01-11 00:00:55 +0530

    Create completeTask.py

---
+++ completeTask.py
@@ -0,0 +1,24 @@
+def  project102():
+    print("Five steps for a successful project")
+step = input("whats step number- ")
+if(step == '1'):
+    print("Step 1: Starting")
+    print("Decide the theme for your project")
+
+elif(step == '2'):
+    print("Step 2: Negotiation.")
+    print("Once you've got your ideas, you need to make sure they are acceptable to your audience")
+
+elif(step == '3'):
+    print("Step 3: Time and Resources")
+    print("By this point, you will probably be working with only the lightest supervision, so you really need to pay attention to what you actually need to do")
+
+elif(step == '4'):
+    print("Step 4: Outcome")
+    print("After all your work, you will, eventually, finish your project. your teacher.")
+
+elif(step == '5'):
+    print("Step 5: Judgement")
+    print("Once you have presented your final piece of work, it will be judged. Hopefully, you will have succeeded")
+
+project102() 
